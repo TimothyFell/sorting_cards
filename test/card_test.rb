7a00ec32fea2f388d2484d6054909f370d0ac212
TimothyFell <timothy.fell846@gmail.com>
2018-08-19 11:46:23 -0600

    Create card class test

--- test/card_test.rb
+++ test/card_test.rb
@@ -2,3 +2,22 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
 require './lib/card'
+
+class CardTest < Minitest::Test
+
+  def test_it_exists
+    two_clubs = Card.new("2", "Clubs")
+    assert_instance_of Card, two_clubs
+  end
+
+  def test_it_has_value
+    two_clubs = Card.new("2", "Clubs")
+    assert_equal "2", two_clubs.value
+  end
+
+  def test_it_has_suit
+    two_clubs = Card.new("2", "Clubs")
+    assert_equal "Clubs", two_clubs.suit
+  end
+
+end
