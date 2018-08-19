2c1aa0fc3da69ca22f744d8b0a24233d91c11faa
TimothyFell <timothy.fell846@gmail.com>
2018-08-19 12:21:46 -0600

    Write deck tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,3 +4,30 @@ require 'pry'
 require './lib/card'
 require './lib/guess'
 require './lib/deck'
+
+class DeckTest < Minitest::Test
+
+  def test_it_exists
+    test_deck = Deck.new()
+    assert_instance_of Deck, test_deck
+  end
+
+  def test_it_has_cards
+    two_clubs = Card.new("2", "Clubs")
+    three_hearts = Card.new("2", "Hearts")
+    ace_spades = Card.new("2", "Spades")
+    test_deck = Deck.new([two_clubs, three_hearts, ace_spades])
+    expected = [two_clubs, three_hearts, ace_spades]
+    assert_equal expected, test_deck.cards
+  end
+
+  def test_it_can_count_cards
+    skip
+    two_clubs = Card.new("2", "Clubs")
+    three_hearts = Card.new("2", "Hearts")
+    ace_spades = Card.new("2", "Spades")
+    test_deck = Deck.new([two_clubs, three_hearts, ace_spades])
+    assert_equal 3, test_deck.count
+  end
+
+end
