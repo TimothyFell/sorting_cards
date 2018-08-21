9dec42b72bdb6472780cb3b3356200fdabce47f2
TimothyFell <timothy.fell846@gmail.com>
2018-08-21 16:46:24 -0600

    Remove erroneous codefrom files

--- lib/card.rb
+++ lib/card.rb
@@ -24,14 +24,3 @@ suit = "Diamonds"
   end
 
 end
-
-card_1 = Card.new("4","Hearts")
-card_2 = Card.new("Jack", "Clubs")
-card_3 = Card.new("5", "Diamonds")
-card_4 = Card.new("Ace", "Spades")
-card_5 = Card.new("Ace", "Diamonds")
-p card_1.card_id
-p card_2.card_id
-p card_3.card_id
-p card_4.card_id
-p card_5.card_id
--- test/card_test.rb
+++ test/card_test.rb
@@ -6,18 +6,25 @@ require './lib/card'
 class CardTest < Minitest::Test
 
   def test_it_exists
-    two_clubs = Card.new("10", "Hearts")
-    assert_instance_of Card, two_clubs
+    hearts_10 = Card.new("10", "Hearts")
+    assert_instance_of Card, hearts_10
   end
 
   def test_it_has_value
-    two_clubs = Card.new("10", "Hearts")
-    assert_equal "10", two_clubs.value
+    hearts_10 = Card.new("10", "Hearts")
+    assert_equal "10", hearts_10.value
   end
 
   def test_it_has_suit
-    two_clubs = Card.new("10", "Hearts")
-    assert_equal "Hearts", two_clubs.suit
+    hearts_10 = Card.new("10", "Hearts")
+    assert_equal "Hearts", hearts_10.suit
+  end
+
+  def test_card_can_create_unique_card_id
+    hearts_10 = Card.new("10", "Hearts")
+    spades_king = Card.new("King", "Spades")
+    assert_equal 36, hearts_10.card_id
+    assert_equal 49, spades_king.card_id
   end
 
 end
